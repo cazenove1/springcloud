ca58c14098ed1c27dbf76271becad1e61a5a5442
srpingcloud <Cazen_java@163.cm>
2018-07-12 11:35:28 +0800

    springcloud的实例Demo

---
+++ serice-feign/src/main/java/com/caiduoduo/controller/Test.java
@@ -0,0 +1,12 @@
+package com.caiduoduo.controller;
+
+/**
+ * @Auhtor: Cazen
+ * @Description:
+ * @version: Created in 11:34 2018/7/12
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
